e4bf16c79d81ad0538c29f03032c9955e337dbdb
Kugatheesan <vkugatheesan@gmail.com>
2024-10-08 05:06:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const options  = [
+{value:'srilanka', text:'srilanka' , url: 'http://universities.hipolabs.com/search?country=${country}'}
+];
+
+let select= document.getElementById("STC")
+function update(){
+    
+}
